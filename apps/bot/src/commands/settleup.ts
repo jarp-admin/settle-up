42b7752de26e1a406673f5761406bd6b0c0d8275
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-02 00:44:00 +0100

    example button response created

--- apps/bot/src/commands/settleup.ts
+++ apps/bot/src/commands/settleup.ts
@@ -1,4 +1,11 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Message, SlashCommandBuilder } from "discord.js";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Events,
+  Message,
+  SlashCommandBuilder,
+} from "discord.js";
 import { Command } from "../types";
 
 let settleup: Command = {
@@ -8,12 +15,10 @@ let settleup: Command = {
     .addUserOption((option) =>
       option.setName("user").setDescription("user to ping").setRequired(true)
     ),
-    
 
   handler: async (i) => {
     let target = i.options.getUser("user");
-    const button = new ActionRowBuilder<ButtonBuilder>()
-    .addComponents(
+    const button = new ActionRowBuilder<ButtonBuilder>().addComponents(
       new ButtonBuilder()
         .setCustomId("User 1")
         .setLabel(i.options.getUser("user")?.username || "")
@@ -21,19 +26,28 @@ let settleup: Command = {
       new ButtonBuilder()
         .setCustomId("User 2")
         .setLabel(i.user.tag || "")
-        .setStyle(ButtonStyle.Primary)/*,
+        .setStyle(ButtonStyle.Primary) /*,
       new ButtonBuilder()
         .setCustomId("link")
         .setLabel("Payment Link")
         .setStyle(ButtonStyle.Link)//*/
-    )
+    );
     // if get iowethem > 0:
     const Response = `You are going to pay ${target} `; /*amount of money`*/
     // if iowethem = 0 and theyoweme > 0:
     //  const Response =  `${target} owes you `/*amount of money, get them to pay you instead`*/
     // else:
     //  const Response =  `You and ${target} are squared up, you don't need to transfer money at the moment`
-    await i.reply({ content: Response  + " select the button with your name once you have confirmed the transaction, payee use the link to access the payment", components: [button] /**/});
+    let msg = await i.reply({
+      content:
+        Response +
+        " select the button with your name once you have confirmed the transaction, payee use the link to access the payment",
+      components: [button] /**/,
+    });
+
+    msg.createMessageComponentCollector().on("collect", async (i) => {
+      i.reply("yes");
+    });
   },
 };
 
