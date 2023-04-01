97ce715a830dccd57bc042c134af3ff443d980f3
Ashleen <2552805d@student.gla.ac.uk>
2023-04-01 15:38:54 +0100

    started tab procedures

--- apps/server/routers/tab.ts
+++ apps/server/routers/tab.ts
@@ -5,4 +5,13 @@ export const tabRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.tab.findMany();
   }),
+  addOrCreate: publicProcedure
+    .input(
+      z.object({
+        amount: z.number(),
+        debtorId: z.number(),
+        creditorId: z.number(),
+      })
+    )
+    .mutation(({ input, ctx }) => {}),
 });
