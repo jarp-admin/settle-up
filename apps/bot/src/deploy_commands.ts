ef281befdc849364b7be42987a948c2dfbfcca31
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-01 15:26:39 +0100

    first command added and registered

---
+++ apps/bot/src/deploy_commands.ts
@@ -0,0 +1,47 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import path from "path";
+import fs from "fs";
+import { REST, Routes } from "discord.js";
+
+const commands = [];
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN as string);
+
+// and deploy your commands!
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID as string,
+        process.env.GUILD_ID as string
+      ),
+      { body: commands }
+    );
+
+    console.log(
+      // @ts-ignore
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
+})();
--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,16 +1,68 @@
-import * as dotenv from 'dotenv' 
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
-import {Client, Events, GatewayIntentBits} from "discord.js"
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import path from "path";
+import fs from "fs";
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
 
-const client = new Client({intents:[GatewayIntentBits.Guilds]});
+client.once(Events.ClientReady, (e) => {
+  console.log(`Ready! logged in as ${e.user.tag}`);
+});
 
-client.once(Events.ClientReady, e=>{
-    console.log(`Ready! logged in as ${e.user.tag}`)
-})
+let commands = new Collection();
 
-const TOKEN = process.env.TOKEN
+const commandPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandPath)
+  .filter((f) => f.endsWith(".js"));
 
-console.log(TOKEN)
-client.login(TOKEN)
+for (let file of commandFiles) {
+  const filePath = path.join(commandPath, file);
+  const command = require(filePath);
+  if ("data" in command && "execute" in command) {
+    commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  //escape non-commands
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    // @ts-ignore
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command",
+        ephemeral: true,
+      });
+    }
+  }
+
+  console.log(interaction);
+});
+
+client.login(process.env.TOKEN);
