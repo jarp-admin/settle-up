d7c83726ece33892673dc42b517d971dad884a01
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-01 18:29:11 +0100

    registered display

--- apps/bot/src/commands/display.ts
+++ apps/bot/src/commands/display.ts
@@ -1,7 +1,7 @@
 import { SlashCommandBuilder } from "discord.js";
 import { Command } from "../../types";
 
-let ping: Command = {
+let display: Command = {
   command: new SlashCommandBuilder()
     .setName("display")
     .setDescription("Shows how much you owe someone")
@@ -12,13 +12,13 @@ let ping: Command = {
   handler: async (i) => {
     let target = i.options.getUser("user");
     // if get iowethem > 0:
-    const Response = `You owe ${target} `/*amount of money`*/
+    const Response = `You owe ${target} `; /*amount of money`*/
     // if iowethem = 0 and theyoweme > 0:
     //  const Response =  `${target} owes you `/*amount of money`*/
     // else:
     //  const Response =  `You and ${target} are squared up`
-    await i.reply({content: Response});
+    await i.reply({ content: Response });
   },
 };
 
-export default ping;
+export default display;
