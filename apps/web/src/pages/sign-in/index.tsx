26e2a04312e53344eeef21126a043e6cddc6bbd7
Petros Kitazos <petroskitazos@gmail.com>
2023-04-01 23:02:55 +0100

    styling for successful sign-up page

--- apps/web/src/pages/sign-in/index.tsx
+++ apps/web/src/pages/sign-in/index.tsx
@@ -6,13 +6,19 @@ const SignIn: NextPage = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-slate-950">
       <div>
-        <button
-          className="rounded-full bg-sky-400 px-6 py-4 text-xl font-semibold text-blue-950 transition-all duration-200 hover:scale-105 hover:bg-sky-500 hover:text-white"
-          onClick={() => signIn()}
-        >
-          Sign In
-        </button>
-        {session?.user && <h1 className="text-white">{session.user.name}</h1>}
+        {session?.user ? (
+          <h1 className="font-mono text-xl text-white">
+            Welcome <span className="text-sky-400">{session.user.name}</span>
+            to <span className="font-bold">settle-up</span>
+          </h1>
+        ) : (
+          <button
+            className="rounded-full bg-sky-400 px-6 py-4 font-mono text-xl font-semibold text-blue-950 transition-all duration-200 hover:scale-105 hover:bg-sky-500 hover:text-white"
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </div>
   );
