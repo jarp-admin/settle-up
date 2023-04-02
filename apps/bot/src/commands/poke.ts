e0eef53547e911223cef6d4f36c7793c5821fa8b
Rory McDermid <2566302M@student.gla.ac.uk>
2023-04-02 01:24:49 +0100

    added poke

---
+++ apps/bot/src/commands/poke.ts
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../types";
+
+let poke: Command = {
+  command: new SlashCommandBuilder()
+    .setName("poke")
+    .setDescription("Reminds someone to pay their tab")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("user to ping")
+    ),
+
+  handler: async (i) => {
+    let target = i.options.getUser("user");
+    await i.reply({ content: `${target} pay your tab to ${i.user.tag}` });
+  },
+};
+
+export default poke;
