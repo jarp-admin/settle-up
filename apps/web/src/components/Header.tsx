ca6a30096cb30d9bcf7f8b92dd13177c39707548
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-04-02 01:51:06 +0100

    more basic UI work

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -3,8 +3,8 @@ import Profile from "./Profile";
 
 const Header: FC = () => {
   return (
-    <div>
-      <h1>Settle-up!</h1>
+    <div className="fixed flex h-20 w-full flex-row items-center justify-between bg-[#FFC517] p-2">
+      <h1 className="font-mono text-xl text-black">Settle-up!</h1>
       <Profile />
     </div>
   );
