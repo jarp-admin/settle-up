8fac42d66a0a298c766f823bf795b966c9f2a4a2
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-05-29 13:17:28 +0100

    wrote command helper methods and associated types

---
+++ apps/bot/src/lib/command.ts
@@ -0,0 +1,77 @@
+import { RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord-api-types/v10";
+import {
+  APIApplicationCommandOption,
+  CacheType,
+  ChatInputCommandInteraction,
+  ApplicationCommandOptionType as optTypes,
+} from "discord.js";
+import { command, commandMeta, handlerOf, option } from "../types/commands";
+
+export const toJSON = ({
+  meta,
+}: command): RESTPostAPIChatInputApplicationCommandsJSONBody => {
+  return {
+    ...meta,
+    options: Object.entries(meta.options).map(([k, v]) => {
+      return {
+        name: k,
+        ...v,
+      } satisfies APIApplicationCommandOption;
+    }),
+  };
+};
+
+export const makeCommand = <T extends Record<string, option>>(
+  meta: commandMeta<T>,
+  handler: handlerOf<T>
+) => {
+  return { meta, handler: destructureArgs(meta, handler) } as command;
+};
+
+type destructureArgs = <T extends Record<string, option>>(
+  meta: commandMeta<T>,
+  handler: handlerOf<T>
+) => handlerOf<T>;
+
+export const destructureArgs: destructureArgs =
+  (meta, handler) => async (i, _) => {
+    let args = Object.keys(meta.options).reduce((acc, optionName) => {
+      let value = getArg(i, optionName, meta.options[optionName]!);
+      // todo handle bad args here
+      // i.e. what if its required and not present?
+
+      return {
+        ...acc,
+        [optionName]: value,
+      };
+    }, {});
+
+    return await handler(i, args as any);
+  };
+
+const getArg = (
+  i: ChatInputCommandInteraction<CacheType>,
+  argName: string,
+  data: option
+) => {
+  switch (data.type) {
+    case optTypes.Boolean:
+      return i.options.getBoolean(argName);
+    case optTypes.String:
+      return i.options.getString(argName);
+    case optTypes.Integer:
+      return i.options.getInteger(argName);
+    case optTypes.Number:
+      return i.options.getNumber(argName);
+    case optTypes.User:
+      return i.options.getUser(argName);
+    case optTypes.Role:
+      return i.options.getRole(argName);
+    case optTypes.Mentionable:
+      return i.options.getMentionable(argName);
+    case optTypes.Channel:
+      return i.options.getChannel(argName);
+    case optTypes.Attachment:
+      return i.options.getAttachment(argName);
+  }
+};
---
+++ apps/bot/src/types/commands.d.ts
@@ -0,0 +1,129 @@
+import {
+  APIApplicationCommandOptionChoice,
+  APIInteractionDataResolvedChannel,
+  APIInteractionDataResolvedGuildMember,
+  APIRole,
+  Attachment,
+  CacheType,
+  CategoryChannel,
+  ChatInputCommandInteraction,
+  ForumChannel,
+  GuildMember,
+  NewsChannel,
+  PrivateThreadChannel,
+  PublicThreadChannel,
+  Role,
+  StageChannel,
+  TextChannel,
+  User,
+  VoiceChannel,
+  ApplicationCommandOptionType as optTypes,
+} from "discord.js";
+
+type optionBase = { required?: boolean; description: string };
+
+type stringOption = optionBase & {
+  type: optTypes.String;
+  max_length?: number;
+  min_length?: number;
+} & (
+    | {
+        choices?: APIApplicationCommandOptionChoice<string>[];
+        autocomplete?: false;
+      }
+    | { autocomplete: true }
+  );
+
+type numberOption = optionBase & {
+  type: optTypes.Number;
+  min?: number;
+  max?: number;
+} & (
+    | {
+        choices?: APIApplicationCommandOptionChoice<number>[];
+        autocomplete?: false;
+      }
+    | { autocomplete: true }
+  );
+
+type integerOption = optionBase & {
+  type: optTypes.Integer;
+  min?: number;
+  max?: number;
+} & (
+    | {
+        choices?: APIApplicationCommandOptionChoice<number>[];
+        autocomplete?: false;
+      }
+    | { autocomplete: true }
+  );
+
+type booleanOption = optionBase & { type: optTypes.Boolean };
+
+type userOption = optionBase & { type: optTypes.User };
+type roleOption = optionBase & { type: optTypes.Role };
+type mentionableOption = optionBase & { type: optTypes.Mentionable };
+type channelOption = optionBase & { type: optTypes.Channel };
+
+type attachmentOption = optionBase & { type: optTypes.Attachment };
+
+type option =
+  | stringOption
+  | numberOption
+  | integerOption
+  | booleanOption
+  | userOption
+  | roleOption
+  | mentionableOption
+  | channelOption
+  | attachmentOption;
+
+type getArgType<T> = T extends optTypes.String
+  ? string
+  : T extends optTypes.Number
+  ? number
+  : T extends optTypes.Integer
+  ? number
+  : T extends optTypes.Boolean
+  ? boolean
+  : T extends optTypes.User
+  ? User
+  : T extends optTypes.Role
+  ? Role | APIRole
+  : T extends optTypes.Mentionable
+  ? User | GuildMember | APIInteractionDataResolvedGuildMember | Role | APIRole
+  : T extends optTypes.Channel
+  ?
+      | CategoryChannel
+      | NewsChannel
+      | StageChannel
+      | TextChannel
+      | PrivateThreadChannel
+      | PublicThreadChannel<boolean>
+      | VoiceChannel
+      | ForumChannel
+      | APIInteractionDataResolvedChannel
+  : T extends optTypes.Attachment
+  ? Attachment
+  : never;
+
+//*   actual interesting types:
+export interface commandMeta<T extends Record<string, option>> {
+  name: string;
+  description: string;
+  options: T;
+}
+
+export type argsFor<T extends Record<string, option>> = {
+  [a in keyof T]: getArgType<T[a]["type"]>;
+};
+
+export type handlerOf<T extends Record<string, option>> = (
+  i: ChatInputCommandInteraction<CacheType>,
+  args: argsFor<T>
+) => void | Promise<void>;
+
+export interface command {
+  meta: commandMeta<Record<string, option>>;
+  handler: handlerOf<Record<string, option>>;
+}
