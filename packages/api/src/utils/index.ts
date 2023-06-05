dc4bb13de8be203e2b9f6589461e7169fb142b1e
Ashleen <2552805d@student.gla.ac.uk>
2023-06-05 10:34:11 +0100

    small change

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as getDateDiff } from "./dateDiff";
 export { default as generatePaypalLink } from "./payments";
-export { default as fetchOrUpdateErrorHandler } from "./prismaErrorHandler";
 export { default as prismaErrorHandler } from "./prismaErrorHandler";
