47461a8a464fc9f7c8c5d501a9bfb8a6e73d1151
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-06-12 22:16:22 +0100

    removed void keyword from signIn

--- apps/web/src/components/SignIn.tsx
+++ apps/web/src/components/SignIn.tsx
@@ -5,7 +5,8 @@ const SignIn: FC = () => {
   return (
     <button
       className="rounded-full bg-sky-400 px-6 py-3 font-mono text-xl font-semibold text-blue-950 transition-all duration-200 hover:scale-105 hover:bg-sky-500 hover:text-white"
-      onClick={() => void signIn()}
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      onClick={() => signIn()}
     >
       Sign In
     </button>
